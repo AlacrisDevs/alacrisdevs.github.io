348098681a498a65466535378fb060208ee05cbd
Alacris <alacrisdevs@gmail.com>
2024-11-11 13:19:23 +0200

    Fixed one image path

--- src/components/lists/EventsData.js
+++ src/components/lists/EventsData.js
@@ -7,7 +7,7 @@ export const eventList = [
     },
       {
         src: '/images/events/event_gamedev_guild.png', 
-        url: 'galleryList',
+        url: 'https://www.linkedin.com/posts/alacrisdevs_v10-of-gamedev-guild-is-a-go-weve-graduated-activity-7252672283402534912-Iud0/',
         name: 'Gamedev Guild',
         description: 'A casual meetup for Estonian gamedevs to showcase projects in an informal setting.'
     },
