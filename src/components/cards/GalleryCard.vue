d212a8f05bacc485855d41b2db7fdf32df4238ff
Alacris <alacrisdevs@gmail.com>
2024-11-12 11:09:31 +0200

    Slightly moved the gallery view buttons, no easy way to make mobile landscape view better

--- src/components/cards/GalleryCard.vue
+++ src/components/cards/GalleryCard.vue
@@ -56,7 +56,7 @@ const showPreviousImage = () => {
 
         <h3 class="text-light">{{ galleryList[currentImageIndex].name }}</h3>
 
-        <button @click.prevent="toggleOverlay" class="p-2 absolute top-24 right-6 z-[2]">
+        <button @click.prevent="toggleOverlay" class="p-2 absolute top-20 right-4 xl:top-24 xl:right-6 z-[2]">
             <svg class="w-8 xl:w-16 h-8 xl:h-16" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" stroke="#E9F2FF" stroke-width="4">
                     <line x1="5" y1="5" x2="27" y2="27" />
@@ -65,14 +65,14 @@ const showPreviousImage = () => {
             </svg>
         </button>
 
-        <button v-if="currentImageIndex > 0" @click.prevent="showPreviousImage" class="p-2 absolute left-6 z-[2]">
+        <button v-if="currentImageIndex > 0" @click.prevent="showPreviousImage" class="p-2 absolute left-4 xl:left-6 z-[2]">
             <svg class="w-8 xl:w-16 h-8 xl:h-16" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 5L10 16L21 27" stroke="#E9F2FF" stroke-width="4" />
             </svg>
         </button>
 
         <button v-if="currentImageIndex < galleryList.length - 1" @click.prevent="showNextImage"
-            class="p-2 absolute right-6 z-[2]">
+            class="p-2 absolute right-4 xl:right-6 z-[2]">
             <svg class="w-8 xl:w-16 h-8 xl:h-16" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10 27L21 16L10 5" stroke="#E9F2FF" stroke-width="4" />
             </svg>
