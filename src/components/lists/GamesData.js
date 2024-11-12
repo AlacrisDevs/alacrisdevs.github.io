de9b13170b3cf25f35131b5c313dfd879e722e4f
Alacris <alacrisdevs@gmail.com>
2024-11-12 17:32:50 +0200

    Apparently foreign characters don't parse in Vue lolers

--- src/components/lists/GamesData.js
+++ src/components/lists/GamesData.js
@@ -2,13 +2,13 @@ export const gameList = [
     {
         src: '/images/games/game_packet_tracers.svg', 
         url: 'https://alacrisdevs.itch.io/packet-tracers',
-        name: 'Packet&nbsp;Tracers',
+        name: 'Packet Tracers',
         description: 'Responsible for all the visuals and sound.'
     },
     {
         src: '/images/games/game_lux_mundi.svg', 
         url: 'https://alacrisdevs.itch.io/lux-mundi',
-        name: 'Lux&nbsp;Mundi',
+        name: 'Lux Mundi',
         description: 'Solo-made project done during a camp.'
     },
     {
