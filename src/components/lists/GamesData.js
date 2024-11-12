3929d7df68b1d11ec63895bb9976de5e02760372
Alacris <alacrisdevs@gmail.com>
2024-11-12 13:31:42 +0200

    Added responsibilities under games

--- src/components/lists/GamesData.js
+++ src/components/lists/GamesData.js
@@ -3,24 +3,24 @@ export const gameList = [
         src: '/images/games/game_packet_tracers.svg', 
         url: 'https://alacrisdevs.itch.io/packet-tracers',
         name: 'Packet Tracers',
-        description: 'Defend your network from the impending viral invasion!'
+        description: 'Responsible for all the visuals and sound.'
     },
     {
         src: '/images/games/game_lux_mundi.svg', 
         url: 'https://alacrisdevs.itch.io/lux-mundi',
         name: 'Lux Mundi',
-        description: 'A visually soothing time-killer sandbox made in 4 days.'
+        description: 'Solo-made project done during a camp.'
     },
     {
         src: '/images/games/game_dichotomia.svg', 
         url: 'https://alacrisdevs.itch.io/dichotomia',
         name: 'Dichotomia',
-        description: 'Fight your inner demon, Jekyll. Fight it.'
+        description: 'Responsible for all the visuals and sound.'
     },
     {
         src: '/images/games/game_awe.svg',
         url: 'https://induo.dev/',
         name: 'At World\'s End',
-        description: 'A race against time, where time itself is your greatest ally and enemy.'
+        description: '[Currently in the works]'
     },
 ];
