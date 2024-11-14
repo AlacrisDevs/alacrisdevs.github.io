b0f640896f48605e41db4e114286eea4167def57
Alacris <alacrisdevs@gmail.com>
2024-11-14 19:39:37 +0200

    Updated GameCamp's description

--- src/components/lists/EventsData.js
+++ src/components/lists/EventsData.js
@@ -3,7 +3,7 @@ export const eventList = [
         src: '/images/events/event_gamecamp.png', 
         url: 'https://gamecamp.ituk.ee/',
         name: 'TalTech GameCamp',
-        description: 'A combination of a game development bootcamp and a game jam.'
+        description: 'Estonia\'s biggest educational game development event/competition.'
     },
       {
         src: '/images/events/event_gamedev_guild.png', 
